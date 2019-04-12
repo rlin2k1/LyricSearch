9e404e1e0e2f362ee2fc9a99141c3b816559ed0a
Roy Lin <rlin2k1@gmail.com>
2019-04-12 15:54:28 -0700

    Get rid of searchengine identifier

--- sql/schema.sql
+++ sql/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE songs(
     song_name varchar(500) NOT NULL, --Need to Change Length Later
     url_suffix varchar(500), --Need to Change Length Later
     PRIMARY KEY(song_id),
-    FOREIGN KEY (artist_id) REFERENCES searchengine.artists (artist_id)
+    FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
 );
 
 DROP TABLE IF EXISTS tokens CASCADE;
@@ -21,7 +21,7 @@ CREATE TABLE tokens(
     token varchar(500) NOT NULL, --Need to Change Length Later
     token_count smallint,
     PRIMARY KEY (song_id, token),
-    FOREIGN KEY (song_id) REFERENCES searchengine.songs (song_id)
+    FOREIGN KEY (song_id) REFERENCES songs (song_id)
 );
 
 DROP TABLE IF EXISTS TFIDFs CASCADE;
@@ -30,6 +30,6 @@ CREATE TABLE TFIDFs(
     token varchar(500) NOT NULL,
     TFIDF_score real NOT NULL, --Need to Change Length Later
     PRIMARY KEY (song_id, token),
-    FOREIGN KEY (song_id, token) REFERENCES searchengine.tokens (song_id, token),
-    FOREIGN KEY (song_id) REFERENCES searchengine.songs (song_id)
+    FOREIGN KEY (song_id, token) REFERENCES tokens (song_id, token),
+    FOREIGN KEY (song_id) REFERENCES songs (song_id)
 );
