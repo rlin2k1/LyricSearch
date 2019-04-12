4590bb940f62f70b7e3d2b83d868b45f409c08b6
Roy Lin <rlin2k1@gmail.com>
2019-04-11 22:03:30 -0700

    Got rid of specifying a schema. Made Tables have Public Schema

--- sql/load.sql
+++ sql/load.sql
@@ -1,3 +1,3 @@
-\copy searchengine.artists FROM '/home/cs143/data/artist.csv' WITH DELIMITER ',' QUOTE '"' CSV;
-\copy searchengine.songs FROM '/home/cs143/data/song.csv' WITH DELIMITER ',' QUOTE '"' CSV;
-\copy searchengine.tokens FROM '/home/cs143/data/token.csv' WITH DELIMITER ',' QUOTE '"' CSV;
+\copy artists FROM '/home/cs143/data/artist.csv' WITH DELIMITER ',' QUOTE '"' CSV;
+\copy songs FROM '/home/cs143/data/song.csv' WITH DELIMITER ',' QUOTE '"' CSV;
+\copy tokens FROM '/home/cs143/data/token.csv' WITH DELIMITER ',' QUOTE '"' CSV;
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,33 +1,34 @@
-DROP SCHEMA IF EXISTS searchengine CASCADE;
-CREATE SCHEMA IF NOT EXISTS searchengine;
-
-CREATE TABLE searchengine.artists(
+DROP TABLE IF EXISTS artists CASCADE;
+CREATE TABLE artists(
     artist_id smallint,
-    artist_name varchar(500), --Need to Change Length Later
+    artist_name varchar(500) NOT NULL, --Need to Change Length Later
     PRIMARY KEY (artist_id)
 );
 
-CREATE TABLE searchengine.songs(
+DROP TABLE IF EXISTS songs CASCADE;
+CREATE TABLE songs(
     song_id int,
     artist_id int,
-    song_name varchar(500), --Need to Change Length Later
+    song_name varchar(500) NOT NULL, --Need to Change Length Later
     url_suffix varchar(500), --Need to Change Length Later
     PRIMARY KEY(song_id),
     FOREIGN KEY (artist_id) REFERENCES searchengine.artists (artist_id)
 );
 
-CREATE TABLE searchengine.tokens(
+DROP TABLE IF EXISTS tokens CASCADE;
+CREATE TABLE tokens(
     song_id int,
-    token varchar(500), --Need to Change Length Later
+    token varchar(500) NOT NULL, --Need to Change Length Later
     token_count smallint,
     PRIMARY KEY (song_id, token),
     FOREIGN KEY (song_id) REFERENCES searchengine.songs (song_id)
 );
 
-CREATE TABLE searchengine.TFIDFs(
+DROP TABLE IF EXISTS TFIDFs CASCADE;
+CREATE TABLE TFIDFs(
     song_id int, --Need to Change Length Later
-    token varchar(500),
-    TFIDF_score real, --Need to Change Length Later
+    token varchar(500) NOT NULL,
+    TFIDF_score real NOT NULL, --Need to Change Length Later
     PRIMARY KEY (song_id, token),
     FOREIGN KEY (song_id, token) REFERENCES searchengine.tokens (song_id, token),
     FOREIGN KEY (song_id) REFERENCES searchengine.songs (song_id)
