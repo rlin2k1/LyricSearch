df5f29bfb42a11d4e7d2cb13fdbdb1749c041035
rlin2k1 <rlin2k1@gmail.com>
2019-04-12 16:09:53 -0700

    Added NOT NULL to Relation Attributes

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,32 +1,32 @@
 DROP TABLE IF EXISTS artists CASCADE;
 CREATE TABLE artists(
-    artist_id smallint,
+    artist_id smallint NOT NULL,
     artist_name varchar(500) NOT NULL, --Need to Change Length Later
     PRIMARY KEY (artist_id)
 );
 
 DROP TABLE IF EXISTS songs CASCADE;
 CREATE TABLE songs(
-    song_id int,
-    artist_id int,
+    song_id int NOT NULL,
+    artist_id int NOT NULL,
     song_name varchar(500) NOT NULL, --Need to Change Length Later
-    url_suffix varchar(500), --Need to Change Length Later
+    url_suffix varchar(500) NOT NULL, --Need to Change Length Later
     PRIMARY KEY(song_id),
     FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
 );
 
 DROP TABLE IF EXISTS tokens CASCADE;
 CREATE TABLE tokens(
-    song_id int,
+    song_id int NOT NULL,
     token varchar(500) NOT NULL, --Need to Change Length Later
-    token_count smallint,
+    token_count smallint NOT NULL,
     PRIMARY KEY (song_id, token),
     FOREIGN KEY (song_id) REFERENCES songs (song_id)
 );
 
 DROP TABLE IF EXISTS TFIDFs CASCADE;
 CREATE TABLE TFIDFs(
-    song_id int, --Need to Change Length Later
+    song_id int NOT NULL, --Need to Change Length Later
     token varchar(500) NOT NULL,
     TFIDF_score real NOT NULL, --Need to Change Length Later
     PRIMARY KEY (song_id, token),
