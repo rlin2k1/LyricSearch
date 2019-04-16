68325a7e8d4e527f2597dfe3608370fbd395677a
Moo Jin Kim <moojinkim9@gmail.com>
2019-04-16 00:52:30 -0700

    Changed artist_id from int to smallint. Inserted max string lengths for all varchar(n). Inserted/Removed spaces here and there to match professor's styling conventions.

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,35 +1,35 @@
 DROP TABLE IF EXISTS artists CASCADE;
-CREATE TABLE artists(
+CREATE TABLE artists (
     artist_id smallint NOT NULL,
-    artist_name varchar(500) NOT NULL, --Need to Change Length Later
+    artist_name varchar(44) NOT NULL,
     PRIMARY KEY (artist_id)
 );
 
 DROP TABLE IF EXISTS songs CASCADE;
-CREATE TABLE songs(
+CREATE TABLE songs (
     song_id int NOT NULL,
-    artist_id int NOT NULL,
-    song_name varchar(500) NOT NULL, --Need to Change Length Later
-    url_suffix varchar(500) NOT NULL, --Need to Change Length Later
+    artist_id smallint NOT NULL,
+    song_name varchar(77) NOT NULL,
+    url_suffix varchar(102) NOT NULL,
     PRIMARY KEY(song_id),
-    FOREIGN KEY (artist_id) REFERENCES artists (artist_id)
+    FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
 );
 
 DROP TABLE IF EXISTS tokens CASCADE;
-CREATE TABLE tokens(
+CREATE TABLE tokens (
     song_id int NOT NULL,
-    token varchar(500) NOT NULL, --Need to Change Length Later
+    token varchar(106) NOT NULL,
     token_count smallint NOT NULL,
     PRIMARY KEY (song_id, token),
-    FOREIGN KEY (song_id) REFERENCES songs (song_id)
+    FOREIGN KEY (song_id) REFERENCES songs(song_id)
 );
 
-DROP TABLE IF EXISTS TFIDFs CASCADE;
-CREATE TABLE TFIDFs(
-    song_id int NOT NULL, --Need to Change Length Later
-    token varchar(500) NOT NULL,
-    TFIDF_score real NOT NULL, --Need to Change Length Later
+DROP TABLE IF EXISTS TFIDF_scores CASCADE;
+CREATE TABLE TFIDF_scores (
+    song_id int NOT NULL,
+    token varchar(106) NOT NULL,
+    TFIDF_score real NOT NULL, --TODO: Verify that this is the right type.
     PRIMARY KEY (song_id, token),
-    FOREIGN KEY (song_id, token) REFERENCES tokens (song_id, token),
-    FOREIGN KEY (song_id) REFERENCES songs (song_id)
+    FOREIGN KEY (song_id, token) REFERENCES tokens(song_id, token),
+    FOREIGN KEY (song_id) REFERENCES songs(song_id)
 );
