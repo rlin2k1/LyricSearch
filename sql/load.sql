210955afc04d8a3d2c17248eebf090a1e7e3c541
Moo Jin Kim <moojinkim9@gmail.com>
2019-04-24 19:19:59 -0700

    Re-init Project 1B

--- sql/load.sql
+++
@@ -1,3 +0,0 @@
-\copy artist FROM '/home/cs143/data/artist.csv' DELIMITER ',' QUOTE '"' CSV;
-\copy song   FROM '/home/cs143/data/song.csv'   DELIMITER ',' QUOTE '"' CSV;
-\copy token  FROM '/home/cs143/data/token.csv'  DELIMITER ',' QUOTE '"' CSV;
